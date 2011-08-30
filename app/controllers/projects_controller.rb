b96f9567362f430d0812cc3039fb637ffb2c4d85
AJ <azzzz@gmx.net>
2011-08-30 01:57:12 +0100

    Users should be redirected back to the project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,8 @@
 class ProjectsController < ApplicationController
+  before_filter :find_project, :only => [:show,
+                                         :edit,
+                                         :update,
+                                         :destroy]
   def index
     @projects = Project.all
   end
@@ -19,15 +23,14 @@ class ProjectsController < ApplicationController
   end
   
   def show
-    @project = Project.find(params[:id])
+
   end
   
   def edit
-    @project = Project.find(params[:id])
+
   end
   
   def update
-    @project = Project.find(params[:id])
     if @project.update_attributes(params[:project])
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -38,10 +41,18 @@ class ProjectsController < ApplicationController
   end
   
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     flash[:notice] = "Project has been deleted."
     redirect_to projects_path
   end
   
+  private
+    def find_project
+      @project = Project.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking" +
+                      " for could not be found."
+      redirect_to projects_path
+    end
+    
 end
